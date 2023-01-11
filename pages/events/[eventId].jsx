d56802be2ed4d8d0a7b58718b6964ba81de63e09
Dan <dan.phillips100@gmail.com>
2023-01-11 16:27:23 +0000

    feat: added event detail page with components in event-detail folder

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,3 +1,28 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 export default function EventDetailPage() {
-  return <div>Single Event</div>;
+  const router = useRouter();
+  const { eventId } = router.query;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No event found</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imaggeAlt={event.title}
+      />
+      <EventContent>{event.description}</EventContent>
+    </Fragment>
+  );
 }
